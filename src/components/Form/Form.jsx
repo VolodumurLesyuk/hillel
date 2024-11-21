7136ef8a0d5f4a72946c9a64a0dcbed7f8712161
VolodumurLesyuk <volodumurlesiuk@gmail.com>
2024-11-21 12:35:24 +0200

    Rename state input form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -4,10 +4,10 @@ import Button from '../Button/Button.jsx'
 import {useState} from "react";
 
 const Form = () => {
-    const [value, setValue] = useState('')
+    const [username, setUsername] = useState('')
 
     const handlerInputOnChange = (event) => {
-        setValue(event.target.value);
+        setUsername(event.target.value);
         console.log(event.target.value)
     }
 
@@ -16,7 +16,7 @@ const Form = () => {
             <h1>The best pizza.</h1>
             <p className="subtitle">Straight out of the oven, straight to you.</p>
             <p className="welcome">👉 Welcome! Please start by telling us your name:</p>
-            <Input value={value} onChange={handlerInputOnChange} type="text" placeholder="Your full name" className='form-input' aria_label="Your full name" />
+            <Input value={username} onChange={handlerInputOnChange} type="text" placeholder="Your full name" className='form-input' aria_label="Your full name" />
             <Button className="btn" text={'Start Order'}/>
         </main>
     )
