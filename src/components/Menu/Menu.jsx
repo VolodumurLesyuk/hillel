5c46a1ca62f391b848b62f520ad1182a805e2657
VolodumurLesyuk <volodumurlesiuk@gmail.com>
2024-11-27 11:21:05 +0200

    Add new hook useEffect

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,12 +1,30 @@
 import "./Menu.css";
 import PizzaItem from "../PizzaItem/PizzaItem.jsx";
-import pizzas from "./data.js";
+import {useEffect, useState} from "react";
+
 
 const Menu = () => {
+    const [pizzas, setPizzas] = useState([]);
+    useEffect(() => {
+        const getPizzas = async () => {
+            try {
+                const res = await fetch("https://react-fast-pizza-api.onrender.com/api/menu");
+                if (!res.ok) {
+                    throw new Error("Failed to fetch Pizzas");
+                }
+                const data = await res.json();
+                setPizzas(data.data);
+            } catch (error) {
+                console.error(error.message);
+            }
+        };
+
+        getPizzas();
+    }, []);
     return (
         <div className="menu-container">
             {pizzas.map((pizza) => (
-                <PizzaItem key={pizza.id} pizza={pizza} />
+                <PizzaItem key={pizza.id} pizza={pizza}/>
             ))}
         </div>
     );
